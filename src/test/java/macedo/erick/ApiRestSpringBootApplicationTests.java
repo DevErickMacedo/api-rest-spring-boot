2f0ad3311dd5cb7a0ec5ff0afb824a8bb7c41596
erick <erickmacedo120@outlook.com>
2024-07-23 17:21:13 -0300

    Initial commit

---
+++ src/test/java/macedo/erick/ApiRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package macedo.erick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
